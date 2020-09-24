1d9fcf520b7707c3252f90a169631baf7829d8dc
Suliano, Ethel A <ethel.a.suliano@accenture.com>
2020-09-24 21:52:39 +0800

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.mypackage;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("*****Hello World & Welcome to Jenkins Training*****");
+    }
+}
